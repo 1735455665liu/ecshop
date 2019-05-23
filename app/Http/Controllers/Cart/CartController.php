6c0e8a7bfe956195221952ad0cbe71c79d5431d0
liu <461043365@qq.com>
2019-05-23 07:26:45 +0000

    购物车

---
+++ app/Http/Controllers/Cart/CartController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Cart;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CartController extends Controller
+{
+    //添加购物车
+    public function cartAdd(){
+
+    }
+}
