351d77ce342d1dc5f5716910bc9c5b6c7c46ea07
gxd <17610531571@163.com>
2019-05-23 09:25:20 +0000

    debug

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,9 @@
 Route::get('/','Home\HomeControoler@index');    //首页
 Route::get('product-list.html','Goods\GoodsController@goodslists');    //商品详情
 
-<<<<<<< HEAD
 
 
 Route::post('/cart/cartAdd','Cart\CartController@cartAdd');    //添加购物车
 Route::get('/cart/cartList','Cart\CartController@cartList');    //购物车列表
 
 
-=======
->>>>>>> 2f8d48b4ee79324a10bc4619f5eb22759bbb3382
