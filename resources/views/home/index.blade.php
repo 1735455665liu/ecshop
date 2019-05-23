f514073885b0acec010451db1ef445ff365ebcbc
1735455665liu <1735455665@qq.com>
2019-05-23 16:27:52 +0800

    修改文字

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -2,7 +2,7 @@
 <html lang="zxx">
 <head>
     <meta charset="UTF-8">
-    <title>Mstore - Online Shop Mobile Template</title>
+    <title>烨氏集团 - Online Shop Mobile Template</title>
     <meta name="viewport" content="width=device-width, initial-scale=1  maximum-scale=1 user-scalable=no">
     <meta name="mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-capable" content="yes">
@@ -28,7 +28,7 @@
 <div class="navbar-top">
     <!-- site brand	 -->
     <div class="site-brand">
-        <a href="index.html"><h1>Mstore</h1></a>
+        <a href="index.html"><h1>烨氏集团</h1></a>
     </div>
     <!-- end site brand	 -->
     <div class="side-nav-panel-right">
@@ -367,27 +367,28 @@
 
     <ul class="slides">
         <li>
-            <img src="img/slide1.jpg" alt="">
+            <img src="img/1.jpg" alt="">
             <div class="caption slider-content  center-align">
-                <h2>WELCOME TO MSTORE</h2>
-                <h4>Lorem ipsum dolor sit amet.</h4>
-                <a href="" class="btn button-default">SHOP NOW</a>
+                <h2>欢迎来到烨氏集团</h2>
+                <h4>烨氏出品 必属精品</h4>
+                <a href="" class="btn button-default">点击我抓紧购物吧</a>
             </div>
         </li>
         <li>
-            <img src="img/slide2.jpg" alt="">
+            <img src="img/1.jpg" alt="">
             <div class="caption slider-content center-align">
-                <h2>JACKETS BUSINESS</h2>
-                <h4>Lorem ipsum dolor sit amet.</h4>
-                <a href="" class="btn button-default">SHOP NOW</a>
+                <h2>贴心服务</h2>
+                <h4>七天无理由退换货,购物无忧!</h4>
+                <a href="" class="btn button-default">点击我抓紧购物吧</a>
             </div>
         </li>
         <li>
-            <img src="img/slide3.jpg" alt="">
+            <img src="img/1.jpg" alt="">
             <div class="caption slider-content center-align">
-                <h2>FASHION SHOP</h2>
-                <h4>Lorem ipsum dolor sit amet.</h4>
-                <a href="" class="btn button-default">SHOP NOW</a>
+                <h2>名人名言</h2>
+                <h4>激浊而扬清，废贪而立廉 ——刘子烨
+                </h4>
+                <a href="" class="btn button-default">点击我抓紧购物吧</a>
             </div>
         </li>
     </ul>
@@ -404,8 +405,8 @@
                     <div class="icon">
                         <i class="fa fa-car"></i>
                     </div>
-                    <h6>Free Shipping</h6>
-                    <p>Lorem ipsum dolor sit amet consectetur</p>
+                    <h6>免费送货</h6>
+                    <p>支持终身无理由退货</p>
                 </div>
             </div>
             <div class="col s6">
@@ -413,8 +414,8 @@
                     <div class="icon">
                         <i class="fa fa-dollar"></i>
                     </div>
-                    <h6>Money Back</h6>
-                    <p>Lorem ipsum dolor sit amet consectetur</p>
+                    <h6>退款</h6>
+                    <p>售后无忧</p>
                 </div>
             </div>
         </div>
@@ -424,8 +425,8 @@
                     <div class="icon">
                         <i class="fa fa-lock"></i>
                     </div>
-                    <h6>Secure Payment</h6>
-                    <p>Lorem ipsum dolor sit amet consectetur</p>
+                    <h6>安全付款</h6>
+                    <p>集团采用国际领先加密技术</p>
                 </div>
             </div>
             <div class="col s6">
@@ -433,8 +434,8 @@
                     <div class="icon">
                         <i class="fa fa-support"></i>
                     </div>
-                    <h6>24/7 Support</h6>
-                    <p>Lorem ipsum dolor sit amet consectetur</p>
+                    <h6>全天候支持</h6>
+                    <p>有疑问抓紧联系客服吧!</p>
                 </div>
             </div>
         </div>
@@ -445,8 +446,8 @@
 <!-- quote -->
 <div class="section quote">
     <div class="container">
-        <h4>FASHION UP TO 50% OFF</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid ducimus illo hic iure eveniet</p>
+        <h4>前方高能</h4>
+        <p>全场消费烨公子买单 尖叫声</p>
     </div>
 </div>
 <!-- end quote -->
@@ -466,7 +467,7 @@
                     <img src="/goodsImg/{{$v->goods_img}}" alt="">
                     <h6><a href="">{{$v->goods_name}}</a></h6>
                     <div class="price">
-                        {{$v->self_price}} <span>{{$v->market_price}}</span>
+                        ￥{{$v->self_price}} <span>￥{{$v->market_price}}</span>
                     </div>
                     <button class="btn button-default">点击加入购物车</button>
                     <button class="btn button-default" id="cart">加入收藏</button>
@@ -484,8 +485,8 @@
 <div class="promo section">
     <div class="container">
         <div class="content">
-            <h4>PRODUCT BUNDLE</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit</p>
+            <h4>温馨提示</h4>
+            <p>国际一手货源</p>
             <button class="btn button-default">SHOP NOW</button>
         </div>
     </div>
@@ -507,7 +508,7 @@
                     <img src="/goodsImg/{{$v->goods_img}}" alt="">
                     <h6><a href="">{{$v->goods_name}}</a></h6>
                     <div class="price">
-                        {{$v->self_price}} <span>{{$v->market_price}}</span>
+                        ￥{{$v->self_price}} <span>￥{{$v->market_price}}</span>
                     </div>
                     <button class="btn button-default">加入购物车</button>
                     <button class="btn button-default" id="cart">加入收藏</button>
@@ -537,8 +538,8 @@
 <div class="footer">
     <div class="container">
         <div class="about-us-foot">
-            <h6>Mstore</h6>
-            <p>is a lorem ipsum dolor sit amet, consectetur adipisicing elit consectetur adipisicing elit.</p>
+            <h6>烨氏集团</h6>
+            <p>烨氏集团成立于1984年,1988年进入国际行业,经过三十余年的发展,成为国内领先的集团之一</p>
         </div>
         <div class="social-media">
             <a href=""><i class="fa fa-facebook"></i></a>
@@ -548,7 +549,7 @@
             <a href=""><i class="fa fa-instagram"></i></a>
         </div>
         <div class="copyright">
-            <span>© 2017 All Right Reserved</span>
+            <span>© 2019 版权所有 烨氏集团</span>
         </div>
     </div>
 </div>
