f790de657f839eef81da34fffc61e34445081b01
liyijiehahaha <2632379213@qq.com>
2019-05-24 06:44:37 +0000

    session

--- app/Http/Controllers/Login/LoginController.php
+++ app/Http/Controllers/Login/LoginController.php
@@ -1,11 +1,9 @@
 <?php
 
 namespace App\Http\Controllers\Login;
-
 use App\Model\User\UserInfo;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
 class LoginController extends Controller
 {
     /*登录页面*/
@@ -19,7 +17,7 @@ class LoginController extends Controller
         $res=UserInfo::where(['user_name'=>$user_name])->first()->toArray();
         if($res){//账号存在
             if(password_verify($user_pwd,$res['user_pwd'])){//密码正确
-                session(['user_id' => $res['user_id']]);
+                session(['user_id' => $res['user_id'],'user_name'=>$res['user_name']]);
                 echo '登陆成功,3秒候跳转至首页';
                 header("refresh:3;url=/");
             }else{//密码错误
