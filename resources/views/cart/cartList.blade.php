2deddb0347bdc957ff913b3e0c7135fb6b1483fd
1735455665liu <1735455665@qq.com>
2019-05-25 11:01:44 +0800

    页面修复

--- resources/views/cart/cartList.blade.php
+++ resources/views/cart/cartList.blade.php
@@ -57,10 +57,10 @@
 <div class="navbar-bottom">
     <div class="row">
         <div class="col s2">
-            <a href="index.html"><i class="fa fa-home"></i></a>
+            <a href="/"><i class="fa fa-home"></i></a>
         </div>
         <div class="col s2">
-            <a href="wishlist.html"><i class="fa fa-heart"></i></a>
+            <a href="/colllist.html"><i class="fa fa-heart"></i></a>
         </div>
         <div class="col s4">
             <div class="bar-center">
@@ -87,7 +87,7 @@
         <div class="container">
             <div class="row">
                 <div class="col s4">
-                    <a href="index.html" class="button-link">
+                    <a href="/" class="button-link">
                         <div class="menu-link">
                             <div class="icon">
                                 <i class="fa fa-home"></i>
@@ -107,19 +107,7 @@
                     </a>
                 </div>
                 <div class="col s4">
-                    <a href="shop-single.html" class="button-link">
-                        <div class="menu-link">
-                            <div class="icon">
-                                <i class="fa fa-eye"></i>
-                            </div>
-                            Single Shop
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col s4">
-                    <a href="wishlist.html" class="button-link">
+                    <a href="/colllist.html" class="button-link">
                         <div class="menu-link">
                             <div class="icon">
                                 <i class="fa fa-heart"></i>
@@ -138,113 +126,9 @@
                         </div>
                     </a>
                 </div>
-                <div class="col s4">
-                    <a href="checkout.html" class="button-link">
-                        <div class="menu-link">
-                            <div class="icon">
-                                <i class="fa fa-credit-card"></i>
-                            </div>
-                            Checkout
-                        </div>
-                    </a>
-                </div>
             </div>
-            <div class="row">
-                <div class="col s4">
-                    <a href="blog.html" class="button-link">
-                        <div class="menu-link">
-                            <div class="icon">
-                                <i class="fa fa-bold"></i>
-                            </div>
-                            Blog
-                        </div>
-                    </a>
-                </div>
-                <div class="col s4">
-                    <a href="blog-single.html" class="button-link">
-                        <div class="menu-link">
-                            <div class="icon">
-                                <i class="fa fa-file-text-o"></i>
-                            </div>
-                            Blog Single
-                        </div>
-                    </a>
-                </div>
-                <div class="col s4">
-                    <a href="error404.html" class="button-link">
-                        <div class="menu-link">
-                            <div class="icon">
-                                <i class="fa fa-hourglass-half"></i>
-                            </div>
-                            404
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col s4">
-                    <a href="testimonial.html" class="button-link">
-                        <div class="menu-link">
-                            <div class="icon">
-                                <i class="fa fa-support"></i>
-                            </div>
-                            Testimonial
-                        </div>
-                    </a>
-                </div>
-                <div class="col s4">
-                    <a href="about-us.html" class="button-link">
-                        <div class="menu-link">
-                            <div class="icon">
-                                <i class="fa fa-user"></i>
-                            </div>
-                            About Us
-                        </div>
-                    </a>
-                </div>
-                <div class="col s4">
-                    <a href="contact.html" class="button-link">
-                        <div class="menu-link">
-                            <div class="icon">
-                                <i class="fa fa-envelope-o"></i>
-                            </div>
-                            Contact
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col s4">
-                    <a href="setting.html" class="button-link">
-                        <div class="menu-link">
-                            <div class="icon">
-                                <i class="fa fa-cog"></i>
-                            </div>
-                            Settings
-                        </div>
-                    </a>
-                </div>
-                <div class="col s4">
-                    <a href="login.html" class="button-link">
-                        <div class="menu-link">
-                            <div class="icon">
-                                <i class="fa fa-sign-in"></i>
-                            </div>
-                            Login
-                        </div>
-                    </a>
-                </div>
-                <div class="col s4">
-                    <a href="register.html" class="button-link">
-                        <div class="menu-link">
-                            <div class="icon">
-                                <i class="fa fa-user-plus"></i>
-                            </div>
-                            Register
-                        </div>
-                    </a>
-                </div>
-            </div>
+
+
         </div>
     </div>
 </div>
