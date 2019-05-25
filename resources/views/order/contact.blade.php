4b378b66cf575f5c68e3159b36f2fe30d65a29e7
1735455665liu <1735455665@qq.com>
2019-05-25 15:45:14 +0800

    视图debug

--- resources/views/order/contact.blade.php
+++ resources/views/order/contact.blade.php
@@ -57,7 +57,7 @@
 <div class="navbar-bottom">
     <div class="row">
         <div class="col s2">
-            <a href="index.html"><i class="fa fa-home"></i></a>
+            <a href="/"><i class="fa fa-home"></i></a>
         </div>
         <div class="col s2">
             <a href="wishlist.html"><i class="fa fa-heart"></i></a>
@@ -79,6 +79,8 @@
 <!-- end navbar bottom -->
 
 <!-- menu -->
+
+<!-- end menu -->
 <div class="menus" id="animatedModal2">
     <div class="close-animatedModal2 close-icon">
         <i class="fa fa-close"></i>
@@ -132,118 +134,8 @@
         </div>
     </div>
 </div>
-<!-- end menu -->
-
 <!-- cart menu -->
-<div class="menus" id="animatedModal">
-    <div class="close-animatedModal close-icon">
-        <i class="fa fa-close"></i>
-    </div>
-    <div class="modal-content">
-        <div class="cart-menu">
-            <div class="container">
-                <div class="content">
-                    <div class="cart-1">
-                        <div class="row">
-                            <div class="col s5">
-                                <img src="img/cart-menu1.png" alt="">
-                            </div>
-                            <div class="col s7">
-                                <h5><a href="">Fashion Men's</a></h5>
-                            </div>
-                        </div>
-                        <div class="row quantity">
-                            <div class="col s5">
-                                <h5>Quantity</h5>
-                            </div>
-                            <div class="col s7">
-                                <input value="1" type="text">
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col s5">
-                                <h5>Price</h5>
-                            </div>
-                            <div class="col s7">
-                                <h5>$20</h5>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col s5">
-                                <h5>Action</h5>
-                            </div>
-                            <div class="col s7">
-                                <div class="action"><i class="fa fa-trash"></i></div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="divider"></div>
-                    <div class="cart-2">
-                        <div class="row">
-                            <div class="col s5">
-                                <img src="img/cart-menu2.png" alt="">
-                            </div>
-                            <div class="col s7">
-                                <h5><a href="">Fashion Men's</a></h5>
-                            </div>
-                        </div>
-                        <div class="row quantity">
-                            <div class="col s5">
-                                <h5>Quantity</h5>
-                            </div>
-                            <div class="col s7">
-                                <input value="1" type="text">
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col s5">
-                                <h5>Price</h5>
-                            </div>
-                            <div class="col s7">
-                                <h5>$20</h5>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col s5">
-                                <h5>Action</h5>
-                            </div>
-                            <div class="col s7">
-                                <div class="action"><i class="fa fa-trash"></i></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="total">
-                    <div class="row">
-                        <div class="col s7">
-                            <h5>Fashion Men's</h5>
-                        </div>
-                        <div class="col s5">
-                            <h5>$21.00</h5>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col s7">
-                            <h5>Fashion Men's</h5>
-                        </div>
-                        <div class="col s5">
-                            <h5>$21.00</h5>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col s7">
-                            <h6>Total</h6>
-                        </div>
-                        <div class="col s5">
-                            <h6>$41.00</h6>
-                        </div>
-                    </div>
-                </div>
-                <button class="btn button-default">Process to Checkout</button>
-            </div>
-        </div>
-    </div>
-</div>
+
 <!-- end cart menu -->
 
 <!-- error 404 -->
@@ -274,8 +166,8 @@
 <div class="footer">
     <div class="container">
         <div class="about-us-foot">
-            <h6>Mstore</h6>
-            <p>is a lorem ipsum dolor sit amet, consectetur adipisicing elit consectetur adipisicing elit.</p>
+            <h6>烨氏集团</h6>
+            <p>烨氏集团成立于1984年,1988年进入国际行业,经过三十余年的发展,成为国内领先的集团之一</p>
         </div>
         <div class="social-media">
             <a href=""><i class="fa fa-facebook"></i></a>
@@ -285,7 +177,7 @@
             <a href=""><i class="fa fa-instagram"></i></a>
         </div>
         <div class="copyright">
-            <span>© 2017 All Right Reserved</span>
+            <span>© 2019 版权所有 烨氏集团</span>
         </div>
     </div>
 </div>
