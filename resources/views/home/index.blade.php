235d552123eeda207418408e17d8c229fe4c8592
gxd <17610531571@163.com>
2019-05-27 00:47:29 +0000

    首页

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -50,10 +50,6 @@
         @if(session("user_id")==NULL)
             <li><a href="login.html"><i class="fa fa-sign-in"></i>Login</a></li>
             <li><a href="register.html"><i class="fa fa-user-plus"></i>Register</a></li>
-<<<<<<< HEAD
-            <li><a href="/loginout"><i class="fa fa-sign-in"></i>loginout</a></li>
-=======
->>>>>>> coll
         @else
             <li><a href="/loginout"><i class="fa fa-sign-in"></i>loginout</a></li>
         @endif
