7162505361a981bfdb719fab0b45a0ae76c5c179
liu <461043365@qq.com>
2019-05-27 03:24:34 +0000

    微信授权

--- app/Http/Controllers/Home/HomeControoler.php
+++ app/Http/Controllers/Home/HomeControoler.php
@@ -10,7 +10,18 @@ class HomeControoler extends Controller
 {
 
     //TODO 项目首页
-    public function index(){
+    public function index(Request $request){
+        $code= $request->input('code');
+        if($code){
+            $url1='https://api.weixin.qq.com/sns/oauth2/access_token?appid='.env('WX_APPID').'&secret='.env('WX_APPSECRET').'&code='.$code.'&grant_type=authorization_code';
+            $arr=json_decode(file_get_contents($url1),true);
+            $url2='https://api.weixin.qq.com/sns/userinfo?access_token='.$arr['access_token'].'&openid='.$arr['openid'].'&lang=zh_CN';
+            $userInfo=json_decode(file_get_contents($url2),true);
+            print_r($userInfo);die;
+//            $users=User::where('openid',$userInfo['openid'])->first();
+        }
+
+
         //最新推荐商品
         $GoodsInfo=DB::table('shop_goods')->where(['is_new'=>1])->paginate(4);
 
