0909a035c2ee59e5b22f426d7c03883eac7c7617
gxd <17610531571@163.com>
2019-05-27 02:27:05 +0000

    回调

--- routes/web.php
+++ routes/web.php
@@ -57,3 +57,4 @@ Route::post('/notify','Pay\PayController@notify');    //异步通知
 
 //微信支付
 Route::get('wx/test','Wx\WxPayController@t_test');  //扫码支付
+Route::get('/Weixin/paysuccess', 'Wx\WxPayController@success');      //支付成功
